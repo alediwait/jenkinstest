90ef1275fb00834778bdf99bb4265a89e911ebdd
alediwait <45946482+alediwait@users.noreply.github.com>
2020-02-17 11:23:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Test CICD DWT!')
 })
 
 //Launch listening server on port 8081
 app.listen(8081, function () {
-  console.log('app listening on port 8081!')
+  console.log('La web esta en el puerto 8081!')
 })
